39500a920c1a8f445b26c89b3100dea7f322caa4
Bonnie Liao <kurousagiwasugoi@gmail.com>
2016-03-11 14:14:34 +1300

    Editted index page

--- routes/list.js
+++ routes/list.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('list/browse', { title: 'Colenso Project' });
+  res.render('list/browse', { title: 'Browse' });
 });
 
 module.exports = router;
