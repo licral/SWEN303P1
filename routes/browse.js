4ab949237683feb25a8b52cefe40abbf173cc25f
Bonnie Liao <kurousagiwasugoi@gmail.com>
2016-03-25 16:06:27 +1300

    Search has been refined. can get the document where it came from

--- routes/browse.js
+++ routes/browse.js
@@ -5,11 +5,8 @@ var basex = require('basex');
 var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
 client.execute("OPEN Colenso");
 
-var display;
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	display = "browse";
 	client.execute("XQUERY db:list('Colenso')",
 		function(error, result){
 			if(error){
@@ -18,14 +15,13 @@ router.get('/', function(req, res, next) {
 				res.render('browse', {
 					title: "Browse", 
 					place: result.result.split("\n"), 
-					display: display
+					display: "browse"
 					});
 			}
 		});
 });
 
 router.get('/display/*', function(req, res, next) {
-	display = "human";
 	var search_url = req.url.replace("/display", "Colenso");
 	client.execute("XQUERY doc('" + search_url + "')",
 		function(error, result){
@@ -34,7 +30,7 @@ router.get('/display/*', function(req, res, next) {
 			} else {
 				res.render('browse', {
 					title: "Browse",
-					display: display,
+					display: "human",
 					place: result.result.split("\n")
 					});
 			}
@@ -43,7 +39,6 @@ router.get('/display/*', function(req, res, next) {
 
 
 router.get('/raw-display/*', function(req, res, next) {
-	display = "raw";
 	var search_url = req.url.replace("/raw-display", "Colenso");
 	client.execute("XQUERY doc('" + search_url + "')",
 		function(error, result){
@@ -52,7 +47,7 @@ router.get('/raw-display/*', function(req, res, next) {
 			} else {
 				res.render('browse', {
 					title: "Browse",
-					display: display,
+					display: "raw",
 					place: result.result.split("\n")
 					});
 			}
--- routes/search.js
+++ routes/search.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var cheerio = require('cheerio');
+
 var basex = require('basex');
 var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
 client.execute("OPEN Colenso");
@@ -27,14 +29,68 @@ router.get('/', function(req, res, next) {
 			if(error){
 				console.error(error);
 			} else {
+				var xml_info = [];
+				var place;
+				var type;
 				if(result.result === ""){
 					display = false;
+				} else {
+					var parse = cheerio.load(result.result, {xmlMode: true});
+					var doc_data = parse("TEI");
+					doc_data.each(function(index, element){
+						element = cheerio(element);
+						xml_info.push({
+							title: element.find('title').first().text(),
+							author: element.find('author').first().text(),
+							id: element.attr('xml:id')
+						})
+					})
+					if(xml_info.length > 0){
+						place = xml_info;
+						type = "cheerio";
+					} else {
+						type = "normal";
+						place = result.result.split('\n');
+					}
 				}
 				res.render('search', {
 					title: "Search", 
-					place: result.result.split("\n"),
+					place: place,
 					display: display,
+					type: type,
 					query: search_query
+					});
+			}
+		});
+});
+
+router.get('/display/:id', function(req, res, next) {
+	var id = req.url.split('/');
+	client.execute(xquery + " //TEI[@xml:id='" + req.params.id + "']",
+		function(error, result){
+			if(error){
+				console.error(error);
+			} else {
+				res.render('browse', {
+					title: "Browse",
+					display: "human",
+					place: result.result.split('\n')
+					});
+			}
+		});
+});
+
+router.get('/raw-display/:id', function(req, res, next) {
+	var id = req.url.split('/');
+	client.execute(xquery + " //TEI[@xml:id='" + req.params.id + "']",
+		function(error, result){
+			if(error){
+				console.error(error);
+			} else {
+				res.render('browse', {
+					title: "Browse",
+					display: "raw",
+					place: result.result.split("\n")
 					});
 			}
 		});
